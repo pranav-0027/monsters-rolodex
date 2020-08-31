54f79e34126f62d7625762bc0f33f49a4600e929
Pranav Mitra <pmitra_be19@thapar.edu>
2020-08-31 15:57:09 +0530

    Updates

---
+++ precache-manifest.1350fb7971184e009be9f6266e65e453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2e517f832bf33b07dace27a5adf525",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63fbc1e3c64b2f6d8cc2",
+    "url": "/monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-rolodex/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "63fbc1e3c64b2f6d8cc2",
+    "url": "/monsters-rolodex/static/js/main.51e29502.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
